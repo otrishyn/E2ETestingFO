1e703eb4dabdeea31593985ed56310547b33b178
otrishyn <alexey_trishin@bintime.com>
2018-05-17 13:05:17 +0300

    Demo test

--- README.md
+++ README.md
@@ -2,5 +2,6 @@ $ npm install
 
 $ ./node_modules/.bin/selenium-standalone install --config=selenium-install-config.js
 
+$ ./node_modules/.bin/selenium-standalone start
 
-
+$ ./node_modules/.bin/nightwatch
---
+++ src/Frontoffice/Search/product.js
@@ -0,0 +1,17 @@
+module.exports = {
+  'Find product' : function (browser) {
+    browser
+    .url('https://www.futurumshop.nl')
+    .waitForElementVisible('body', 1000)
+    .setValue('#fc_search', 'futurum essentials bidon 500cc transparant')
+    .click('button.searchButton')
+  },
+
+  'Go to product page' : function (browser) {
+    browser
+    .click('.productCards > .cardContainer > .productCard .productImage > .image > a')
+    .pause(5000)
+    .assert.containsText('#productomschrijving', 'PRODUCTOMSCHRIJVING')
+    .end();
+  }
+};
